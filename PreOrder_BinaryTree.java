3eb7124b2923e0758dd5593245ff5960625bfc6f
arpil_khunt <arpilkhunt1203@gmail.com>
2024-04-21 08:53:32 +0530

    preOrder traversal in binary tree

--- PreOrder_BinaryTree.java
+++ PreOrder_BinaryTree.java
@@ -1,30 +1,43 @@
 public class PreOrder_BinaryTree {
-    static class Node{
+    static class Node {
         int data;
         Node left;
         Node right;
-        Node(int data){
+
+        Node(int data) {
             this.data = data;
-            this.left=null;
-            this.right=null;
+            this.left = null;
+            this.right = null;
         }
     }
 
-    static class BinaryTree{
-        static int idx=-1;
-        public static Node buildTree(int nodes[]){
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
             idx++;
-            if(nodes[idx]==-1){
+            if (nodes[idx] == -1) {
                 return null;
             }
             Node newNode = new Node(nodes[idx]);
-            newNode.left=buildTree(nodes);
-            newNode.right=buildTree(nodes);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
             return newNode;
         }
+
+        // Tree Traversal - PreOrder
+        void preOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
     }
-    public static void main(String args[]){
-        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    public static void main(String args[]) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
