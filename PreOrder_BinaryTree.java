9f006ec6d01393cce417abeabcfdd8e565e5bcc0
arpil_khunt <arpilkhunt1203@gmail.com>
2024-04-23 22:27:24 +0530

    Height of binary tree

--- PreOrder_BinaryTree.java
+++ PreOrder_BinaryTree.java
@@ -88,6 +88,16 @@ public class PreOrder_BinaryTree {
                 }
             }
         }
+
+        // Height of tree
+        int height(Node root) {
+            if (root == null) {
+                return 0;
+            }
+            int lh = height(root.left);
+            int rh = height(root.right);
+            return Math.max(lh, rh) + 1;
+        }
     }
 
     public static void main(String args[]) {
